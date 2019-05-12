5ee4c3caa9ea35a292bac7aeff51f5afc006c450
Nishit Mehta <nishit.mehta1@gmail.com>
2019-05-12 16:26:20 -0500

    Updates

---
+++ precache-manifest.c3c1bf6f1d79d173feadff35e4e8c347.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3562b6c411126a7e9c6fd67ade22c7e8",
+    "url": "/reactjs-redux-blogPost/index.html"
+  },
+  {
+    "revision": "a77b11448ea0846db0d5",
+    "url": "/reactjs-redux-blogPost/static/css/main.2af469a7.chunk.css"
+  },
+  {
+    "revision": "462edc12672f6cd67cea",
+    "url": "/reactjs-redux-blogPost/static/js/2.35ca29d2.chunk.js"
+  },
+  {
+    "revision": "a77b11448ea0846db0d5",
+    "url": "/reactjs-redux-blogPost/static/js/main.e0baa4e2.chunk.js"
+  },
+  {
+    "revision": "576891301fe1b8683280",
+    "url": "/reactjs-redux-blogPost/static/js/runtime~main.8d0fa38e.js"
+  }
+]);
